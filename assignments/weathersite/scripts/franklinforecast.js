9fda8bd631cf779eae01ec0e5dd46398d439db92
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2019-12-17 20:13:27 +0200

    add a forecast script

---
+++ assignments/weathersite/scripts/franklinforecast.js
@@ -0,0 +1,29 @@
+let weatherForcastRequest = new XMLHttpRequest();
+var apiURL = '//api.openweathermap.org/data/2.5/forecast/hourly?id=4156209&APPID=2876382801a396dd5a17e61eabd083ff&units=imperial';
+weatherForcastRequest.open('GET', apiURL, true);
+weatherForcastRequest.send();
+weatherForcastRequest.onload =  function () {
+    let weatherForcastData = JSON.parse(weatherForcastRequest.responseText);
+    console.log(weatherForcastData);
+    /*
+    document.getElementById('maintemp').innerHTML = weatherData.main.temp;
+    document.getElementById('currentweatherdescription').innerHTML = weatherData.weather[0].description;
+    document.getElementById('hightemp').innerHTML = weatherData.main.temp_max;
+    document.getElementById('lowtemp').innerHTML = weatherData.main.temp_min;
+    document.getElementById('wind').innerHTML = weatherData.wind.speed;
+
+    var iconcode = weatherData.weather[0].icon;
+    var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    document.getElementById('weather_icon').src = icon_path;
+
+    var ht = parseFloat(document.getElementById('hightemp').value = weatherData.main.temp_max);
+    var lt = parseFloat(document.getElementById('lowtemp').value = weatherData.main.temp_min);
+    var s = parseFloat(document.getElementById('wind').value = weatherData.wind.speed);
+    /* colculating the average temperature between the high and the low predictions */
+    /*
+    var t = (ht + lt) / 2;
+    var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t *  Math.pow(s, 0.16);
+    f = Math.round(f);
+    document.getElementById("windchill").innerHTML = f;
+    */
+}
