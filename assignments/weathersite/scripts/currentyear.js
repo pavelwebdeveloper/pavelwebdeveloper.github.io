43fe2051361f9539e4f66706347c718b00ed2f61
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2022-06-03 14:24:47 +0200

    Update currentyear.js
    debugging the current year display

--- assignments/weathersite/scripts/currentyear.js
+++ assignments/weathersite/scripts/currentyear.js
@@ -1,2 +1,4 @@
 var date = new Date();
 document.getElementById("currentYear").innerHTML = date.getFullYear();
+console.log("Checking the current year !!!!!!!!!!!!");
+console.log(date.getFullYear());
